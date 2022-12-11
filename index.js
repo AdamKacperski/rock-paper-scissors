599544aa11ca2e3dd7b63450bc6278efdfa39d48
Adam <akacperski96@gmail.com>
2022-12-11 07:15:21 +0100

    added a function which compares the results and declares a winner of a single round

--- index.js
+++ index.js
@@ -1,8 +1,43 @@
-const choices = ['rock', 'paper', 'scissors'];
+const getComputerChoice = () => {
+    const choices = ['rock', 'paper', 'scissors'];
+    const randomNum = [Math.floor(Math.random()*3)];
+    const compChoice = choices[randomNum]
+    return compChoice
+}
+ 
+const playRound = (playerSelection, computerSelection) =>{
 
-function getComputerChoice(){
-    var computer = choices[Math.floor(Math.random()*choices.length)];
-    console.log('Opponent chose ' + computer);
+    if(playerSelection === "rock" && computerSelection === "rock"){
+       return "Draw!" 
+    }
+    else if(playerSelection === "paper" && computerSelection === "paper"){
+        return "Draw!" 
+    }
+    else if(playerSelection === "scissors" && computerSelection === "scissors"){
+        return "Draw!" 
+    }
+    else if(playerSelection === "rock" && computerSelection === "scissors"){
+        return "Player wins!"
+    }
+    else if(playerSelection === "paper" && computerSelection === "rock"){
+        return "Player wins!"
+    }
+    else if(playerSelection === "scissors" && computerSelection === "paper"){
+        return "Player wins!"
+    }
+    else if(playerSelection === "rock" && computerSelection === "paper"){
+        return "Computer wins!"
+    }
+    else if(playerSelection === "paper" && computerSelection === "scissors"){
+        return "Computer wins!"
+    }
+    else if(playerSelection === "scissors" && computerSelection === "rock"){
+        return "Computer wins!"
+    }
 }
 
-getComputerChoice();
+const playerSelection = prompt("rock, paper, scissors?")
+console.log(playerSelection)
+const computerSelection = getComputerChoice()
+console.log(computerSelection)
+console.log(playRound(playerSelection, computerSelection))
